8fb41aa9e36bb9e3a84003e6c86ea60ab6fe4e3e
denys_v <voznyukden23@gmail.com>
2018-07-03 21:20:06 +0300

    add orm and mocha

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,16 +1,14 @@
-const model = require('../models');
+// const model = require('../models');
 
-class MainController {
-    static async test(ctx){
-        let test = 1;
-        // let res = await model.new_table.findAll();
+class MainController{
+	static async test       (ctx)     {
+		// let res = await model.new_table.findAll();
 
+		console.log('fdsdf');
 
-        ctx.body = 1;
+		ctx.body = 1;
 
-
-
-    }
+	}
 }
 
 module.exports = MainController;
