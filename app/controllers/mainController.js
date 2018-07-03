0bfb1a49f48237c629e4198b751a378c473a7b7f
denys_v <voznyukden23@gmail.com>
2018-07-03 21:20:31 +0300

    add orm and mocha

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,7 @@
 // const model = require('../models');
 
 class MainController{
-	static async test       (ctx)     {
+	static async test(ctx){
 		// let res = await model.new_table.findAll();
 
 		console.log('fdsdf');
